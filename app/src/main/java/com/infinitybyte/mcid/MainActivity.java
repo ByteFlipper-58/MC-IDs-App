0f1f85d463a5a08627ee45e9342cde5fd320dce8
Ibragim Maltsagov <ibragim837.tab@gmail.com>
2021-12-29 20:00:12 +0100

    Кароче простите меня англичане, мне лень переводить :moyai:
    1. Добавлен Адаптер (Не работает :moyai::moyai::moyai:)

--- app/src/main/java/com/infinitybyte/mcid/MainActivity.java
+++ app/src/main/java/com/infinitybyte/mcid/MainActivity.java
@@ -1,14 +1,51 @@
 package com.infinitybyte.mcid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.infinitybyte.mcid.adapter.BedrockIDsAdapter;
+import com.infinitybyte.mcid.model.IDsModel;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recview;
+    private BedrockIDsAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recview = (RecyclerView) findViewById(R.id.recview);
+        recview.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+
+        Query query = FirebaseFirestore.getInstance()
+                .collection("bedrock_ids")
+                .orderBy("item_number_id", Query.Direction.ASCENDING);
+
+        FirestoreRecyclerOptions<IDsModel> options = new FirestoreRecyclerOptions.Builder<IDsModel>()
+                .setQuery(query, IDsModel.class)
+                .build();
+
+        adapter = new BedrockIDsAdapter(options);
+        recview.setAdapter(adapter);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        adapter.stopListening();
     }
 }
