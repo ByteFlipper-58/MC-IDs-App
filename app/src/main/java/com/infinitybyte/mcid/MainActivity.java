1c6cc5e4a1f532636a4716fe572c4cc20a16ca8b
Ibragim Maltsagov <ibragim837.tab@gmail.com>
2021-12-29 20:26:41 +0100

    Кароче простите меня англичане, мне лень переводить :moyai:
    1. Добавлен Адаптер (Всё ещё не работает, но в след.комите починю (возможно :moyai::moyai::moyai:) )
    2. Обновлен домашний экран
    3. Доавлен фрагмент для ID'ов Bedrock'a

--- app/src/main/java/com/infinitybyte/mcid/MainActivity.java
+++ app/src/main/java/com/infinitybyte/mcid/MainActivity.java
@@ -10,42 +10,16 @@ import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.infinitybyte.mcid.adapter.BedrockIDsAdapter;
+import com.infinitybyte.mcid.fragment.fragment_bedrock_ids;
 import com.infinitybyte.mcid.model.IDsModel;
 
 public class MainActivity extends AppCompatActivity {
-
-    private RecyclerView recview;
-    private BedrockIDsAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recview = (RecyclerView) findViewById(R.id.recview);
-        recview.setLayoutManager(new LinearLayoutManager(MainActivity.this));
-
-        Query query = FirebaseFirestore.getInstance()
-                .collection("bedrock_ids")
-                .orderBy("item_number_id", Query.Direction.ASCENDING);
-
-        FirestoreRecyclerOptions<IDsModel> options = new FirestoreRecyclerOptions.Builder<IDsModel>()
-                .setQuery(query, IDsModel.class)
-                .build();
-
-        adapter = new BedrockIDsAdapter(options);
-        recview.setAdapter(adapter);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        adapter.startListening();
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        adapter.stopListening();
+        getSupportFragmentManager().beginTransaction().replace(R.id.wrapper, new fragment_bedrock_ids()).commit();
     }
 }
