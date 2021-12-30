a3524cfa64b53d6c80d96925692f9590db1888d3
Ibragim Maltsagov <ibragim837.tab@gmail.com>
2021-12-30 14:01:13 +0100

    Кароче простите меня англичане, мне лень переводить :moyai:
    1. Удален Адаптер
    2. Удалена Модель
    3. Вернул проект в исходное состояние

--- app/src/main/java/com/infinitybyte/mcid/MainActivity.java
+++ app/src/main/java/com/infinitybyte/mcid/MainActivity.java
@@ -1,48 +1,16 @@
 package com.infinitybyte.mcid;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-import com.firebase.ui.firestore.FirestoreRecyclerOptions;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
-import com.infinitybyte.mcid.adapter.BedrockIDsAdapter;
-import com.infinitybyte.mcid.model.IDsModel;
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
-
-    private BedrockIDsAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Query query = FirebaseFirestore.getInstance()
-                .collection("bedrock_ids");
-
-        FirestoreRecyclerOptions<IDsModel> options = new FirestoreRecyclerOptions.Builder<IDsModel>()
-                .setQuery(query, IDsModel.class)
-                .build();
-
-        adapter = new BedrockIDsAdapter(options);
-        RecyclerView recview = findViewById(R.id.recview);
-        recview.setLayoutManager(new LinearLayoutManager(this));
-        recview.setAdapter(adapter);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        adapter.startListening();
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        adapter.stopListening();
     }
 }
